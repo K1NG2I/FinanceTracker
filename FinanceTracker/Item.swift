185725602a4f39e9e3c33ac1385b28f682131061
Yash Kulkarni <kulkarni619y@gmail.com>
2026-02-04 23:28:18 +0530

    Initial Commit

---
+++ FinanceTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FinanceTracker
+//
+//  Created by Yash Kulkarni on 04/02/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ FinanceTrackerTests/FinanceTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerTests.swift
+//  FinanceTrackerTests
+//
+//  Created by Yash Kulkarni on 04/02/26.
+//
+
+import Testing
+@testable import FinanceTracker
+
+struct FinanceTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
